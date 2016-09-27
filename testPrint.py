e09af56dc43fce08e94e4750dd0b0896894f7772
liujun5230 <418873053@qq.com>
2016-09-27 14:46:10 +0800

    second commit of testPrint.py

---
+++ testPrint.py
@@ -0,0 +1,15 @@
+print('hello world')
+print('The quick brown fox','jumps over','the lazy dog')
+print(300)
+print(100+200)
+print('100+200=',100+200)
+
+name = input()
+print(name)
+
+#print absolute value of an integer:
+a = 100
+if a >=0:
+	print(a)
+else:
+	print(-a)
